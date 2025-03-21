2762da62ad7475ab26154fdd874c3af7aa9f9cdc
landerson7 <landerson144000@gmail.com>
2025-03-21 18:29:55 -0400

    Google OAuth tested, basics work, need to fully integrate with app and best practices

--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -3,6 +3,7 @@ import express from "express";
 import { signUp, login } from "../controllers/accounts.controller.js";
 import { addTrainerAccountInfo, getTrainerAccountInfo } from "../controllers/trainerInfo.controller.js";
 import { addClientAccountInfo, getClientAccountInfo } from "../controllers/clientInfo.controller.js";
+import { googleLoginResponse, googleLoginCallback, googleLogout } from "../controllers/google.controller.js";
 
 const router = express.Router();
 
@@ -12,6 +13,10 @@ router.post("/trainer", addTrainerAccountInfo); //where we add trainer account i
 router.get("/trainer", getTrainerAccountInfo); //gets trainer account info  STILL NEEDS TO BE DONE
 router.post("/client", addClientAccountInfo); //where we add trainer account info
 router.get("/client", getClientAccountInfo); //gets trainer account info
+router.get("/auth/google", googleLoginResponse);
+router.get("/auth/google/callback", googleLoginCallback);
+router.get("/auth/google/logout", googleLogout);
+
 
 
 export default router;
