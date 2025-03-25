3a72876579337e6e9d68bca0289ffd5f45b3515d
ethansnead1 <ethan.snead1@gmail.com>
2025-03-24 21:05:21 -0400

    Add files via upload

---
+++ Front End/Components/NavBarData.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import HomeIcon from '@mui/icons-material/Home';
+import CalendarMonthIcon from '@mui/icons-material/CalendarMonth';
+import PostAddIcon from '@mui/icons-material/PostAdd';
+import GroupIcon from '@mui/icons-material/Group';
+import SettingsIcon from '@mui/icons-material/Settings';
+export const NavBarData = [
+{
+    icon: <HomeIcon />,
+    link: "/home"
+},
+
+{
+    icon: <CalendarMonthIcon />,
+    link: "/calender"
+},
+
+{
+    icon: <GroupIcon />,
+    link: "/clients"
+},
+
+{
+    icon: <PostAddIcon />,
+    link: "/programs"
+},
+
+{
+    icon: <SettingsIcon />,
+    link: "/settings"
+},
+
+]
