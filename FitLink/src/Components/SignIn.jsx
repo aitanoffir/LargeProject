3eee92baa7eae850b1569508b5509478e63e1bc4
wcia013twe <wcia013twe@gmail.com>
2025-03-25 17:59:37 -0400

    added sign in/up to main

--- FitLink/src/Components/SignIn.jsx
+++ FitLink/src/Components/SignIn.jsx
@@ -1,8 +1,7 @@
 import React, { useState } from "react";
-import "../index.css";
 import sign_up_picture from "../assets/jonathan-borba-R0y_bEUjiOM-unsplash.jpg";
 
-const SignUp = () => {
+const SignIn = () => {
   const [formData, setFormData] = useState({
     email: "",
     password: "",
@@ -32,11 +31,11 @@ const SignUp = () => {
       <div className="w-full h-screen place-content-center bg-agreeable-grey">
         <div className="w-3/4 h-auto rounded-lg m-auto flex flex-col items-start bg-white">
           <div className="w-full m-0 py-8">
-            <h1 className="text-3xl font-bold text-center mb-2">Sign Up</h1>
-            <p className="text-sm text-center mb">Create your account</p>
+            <h1 className="text-3xl font-bold text-center mb-2">Sign In</h1>
+            <p className="text-sm text-center">Welcome Back</p>
             <form
               onSubmit={handleSubmit}
-              className="w-4/5 m-auto flex flex-col "
+              className="w-4/5 mx-auto flex flex-col"
             >
               <label for="email" className="input-label">
                 Email
@@ -58,25 +57,13 @@ const SignUp = () => {
                 value={formData.password}
                 onChange={handleInputChange}
               />
-              <label for="confirm_password" className="input-label">
-                Confirm Password
-              </label>
-              <input
-                className="input-field"
-                name="confirm_password"
-                type="password"
-                value={formData.confirm_password}
-                onChange={handleInputChange}
-              />
               <button
                 type="submit"
-                className="btn-primary w-full h-10 mt-8 mb-2 rounded-m"
+                className="btn-primary bg-primary w-full h-10 mt-8 mb-2 rounded-m"
               >
-                Submit
+                Sign In
               </button>
-              <p className="text-sm">
-                Already have an account? <a>Log In</a>
-              </p>
+              <p className="text-sm ">Don't have an account yet? Sign Up</p>
             </form>
           </div>
         </div>
@@ -85,4 +72,4 @@ const SignUp = () => {
   );
 };
 
-export default SignUp;
+export default SignIn;
