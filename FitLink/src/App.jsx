50cac41dc420aebc2a3466b89feb5b3a60ee9150
Yessenia Diaz <167342274+yesDiaz1@users.noreply.github.com>
2025-03-29 15:53:49 -0400

    Adding AddClient

--- FitLink/src/App.jsx
+++ FitLink/src/App.jsx
@@ -3,6 +3,7 @@ import './App.css';
 
 import SignUp from './Components/SignUp';
 import SignIn from './Components/SignIn';
+import AddClient from './Components/AddClient';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route path="/" element={<SignIn />} />
         <Route path="/SignUp" element={<SignUp />} />
         <Route path="/SignIn" element={<SignIn />} />
+        <Route path="/AddClient" element={<AddClient />} />
         <Route path="*" element={<Navigate to="/" />} />
       </Routes>
     </Router>
