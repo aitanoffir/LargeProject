5330f7b6b0ff987e61c29b519c88c8af31a2e7b5
Aitan Offir <aitanoffir@Aitans-MacBook-Pro.local>
2025-04-02 17:24:01 -0400

    nav bar

--- FitLink/src/Components/NavBar.jsx
+++ FitLink/src/Components/NavBar.jsx
@@ -1,11 +1,19 @@
-import React from 'react'
+import { FaHome, FaCalendarAlt, FaUserFriends, FaCog, FaUserCircle } from 'react-icons/fa';
 
 const NavBar = () => {
   return (
-    <div>
-      
+    <div className="h-screen w-16 bg-black flex flex-col items-center justify-between py-6">
+      <div className="flex flex-col items-center gap-8">
+        <FaHome className="text-white text-2xl cursor-pointer" />
+        <FaCalendarAlt className="text-white text-2xl cursor-pointer" />
+        <FaUserFriends className="text-white text-2xl cursor-pointer" />
+        <FaCog className="text-white text-2xl cursor-pointer" />
+      </div>
+      <div className="mb-4">
+        <FaUserCircle className="text-white text-3xl cursor-pointer" />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default NavBar
+export default NavBar;
