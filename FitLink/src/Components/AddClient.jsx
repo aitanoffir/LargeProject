616e89fe84aaaecd48088e713935248f01c6304b
Yessenia Diaz <ye177322@ucf.edu>
2025-04-03 12:57:02 -0400

    AddClient

--- FitLink/src/Components/AddClient.jsx
+++ FitLink/src/Components/AddClient.jsx
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
+import "../index.css";
 import { FaBorderAll } from "react-icons/fa";
+import pencil_icon from "../assets/Pencil Icon.png";
 
 const AddClient = () => {
 
@@ -49,9 +51,9 @@ const AddClient = () => {
 
     const rectangleStyle = {
       position: 'absolute',
-      top: '330px',
-      right: '460px',
-      width: '115px', 
+      bottom: '20px',
+      left: '-30px',
+      width: '130px', 
       height: '40px', 
       borderRadius: '22px',
       backgroundColor: '#F18B21', 
@@ -62,29 +64,29 @@ const AddClient = () => {
   return (
     <div className = "flex flex-col p-8">
       <div style={{zindex: 0, backgroundColor: '#F8F8F8', alignItems: 'center',position: "absolute",top: 0, left: 0, width: "100vw", height: "150px", marginLeft: 0, paddingLeft: 0 }}></div>
-      <h1 className= "text-4xl font-bold text-left mb-20 mt-9" style = {{position:'relative', zindex: 1}}>Add a Client</h1>
-      <h2 className = "text-2xl font-bold mb-1 text-left">Enter Client Information</h2>
+      <h1 className= "text-4xl font-bold text-left mb-20 mt-9" style = {{position:'relative', zindex: 1, fontFamily: 'Inter, sans-serif'}}>Add a Client</h1>
+      <h2 className = "text-2xl font-bold mb-1 text-left" style ={{fontFamily: 'Inter, sans-serif'}}>Enter Client Information</h2>
 
       <div className = "w-3/4 bg-white p-8 rounded-lg shadow-lg">
         <div className = "ml-12">
           <div style={containerStyle}>
             <div style={leftColumnStyle}>
-              <label for="name" className = "input-label">Name:</label>
-              <input style={{...columnItemStyle, boxShadow: '0px 1px 3px #2e2e2e', backgroundColor: "#CCCCCC", borderColor: "#734BF4", borderWidth: "2px" }} type = "text" className = "w-65 rounded-lg p-2"/>
+              <label for="name" className = "input-label" style = {{fontFamily: 'Inter, sans-serif'}}>Name:</label>
+              <input style={{...columnItemStyle, boxShadow: '0px 1px 3px #2e2e2e', backgroundColor: "#CCCCCC", borderColor: "#734BF4", borderWidth: "2px", fontFamily: 'Inter, sans-serif'}} type = "text" className = "w-65 rounded-lg p-2"/>
 
-              <label for="DOB" className = "input-label">Date of Birth: (mm/dd/year)</label>
-              <input style={{...columnItemStyle, boxShadow: '0px 1px 3px #2e2e2e', backgroundColor: "#CCCCCC", borderColor: "#734BF4", borderWidth: "2px" }} type = "text" className = "w-65 rounded-lg p-2"/>
+              <label for="DOB" className = "input-label" style = {{fontFamily: 'Inter, sans-serif'}}>Date of Birth: (mm/dd/year)</label>
+              <input style={{...columnItemStyle, boxShadow: '0px 1px 3px #2e2e2e', backgroundColor: "#CCCCCC", borderColor: "#734BF4", borderWidth: "2px", fontFamily: 'Inter, sans-serif'}} type = "text" className = "w-65 rounded-lg p-2"/>
 
-              <label for="weight" className = "input-label">Weight (in pounds):</label>
-              <input style={{...columnItemStyle, boxShadow: '0px 1px 3px #2e2e2e', backgroundColor: "#CCCCCC", borderColor: "#734BF4", borderWidth: "2px" }} type = "text" className = "w-65 rounded-lg p-2"/>
+              <label for="weight" className = "input-label" style = {{fontFamily: 'Inter, sans-serif'}}>Weight (in pounds):</label>
+              <input style={{...columnItemStyle, boxShadow: '0px 1px 3px #2e2e2e', backgroundColor: "#CCCCCC", borderColor: "#734BF4", borderWidth: "2px", fontFamily: 'Inter, sans-serif'}} type = "text" className = "w-65 rounded-lg p-2"/>
 
-              <label for="height" className = "input-label">Height (in inches):</label>
-              <select style={{...columnItemStyle, boxShadow: '0px 1px 3px #2e2e2e', backgroundColor: "#CCCCCC", borderColor: "#734BF4", borderWidth: "2px" }} type = "text" className = "w-65 rounded-lg p-2">
+              <label for="height" className = "input-label" style = {{fontFamily: 'Inter, sans-serif'}}>Height (in inches):</label>
+              <select style={{...columnItemStyle, boxShadow: '0px 1px 3px #2e2e2e', backgroundColor: "#CCCCCC", borderColor: "#734BF4", borderWidth: "2px", fontFamily: 'Inter, sans-serif'}} type = "text" className = "w-65 rounded-lg p-2">
                 <option>Select -Height-</option>
               </select>
 
-              <label for="sex" className = "input-label">Sex:</label>
-              <select style={{...columnItemStyle, boxShadow: '0px 1px 3px #2e2e2e', backgroundColor: "#CCCCCC", borderColor: "#734BF4", borderWidth: "2px" }} type = "text" className = "w-65 rounded-lg p-2">
+              <label for="sex" className = "input-label" style = {{fontFamily: 'Inter, sans-serif'}}>Sex:</label>
+              <select style={{...columnItemStyle, boxShadow: '0px 1px 3px #2e2e2e', backgroundColor: "#CCCCCC", borderColor: "#734BF4", borderWidth: "2px", fontFamily: 'Inter, sans-serif'}} type = "text" className = "w-65 rounded-lg p-2">
                 <option>Select -Sex-</option>
                 <option>Male</option>
                 <option>Female</option>
@@ -93,24 +95,24 @@ const AddClient = () => {
 
             </div>
             <div style={rightColumnStyle}>
-              <label for="email" className="input-label">Email</label>
-              <input style={{...columnItemStyle, boxShadow: '0px 1px 3px #2e2e2e', backgroundColor: "#CCCCCC", borderColor: "#734BF4", borderWidth: "2px" }} type = "text" className = "w-65 rounded-lg p-2"/>
+              <label for="email" className="input-label" style = {{fontFamily: 'Inter, sans-serif'}}>Email</label>
+              <input style={{...columnItemStyle, boxShadow: '0px 1px 3px #2e2e2e', backgroundColor: "#CCCCCC", borderColor: "#734BF4", borderWidth: "2px", fontFamily: 'Inter, sans-serif'}} type = "text" className = "w-65 rounded-lg p-2"/>
 
-              <label for="phone" className = "input-label">Phone Number:</label>
-              <input style={{...columnItemStyle, boxShadow: '0px 1px 3px #2e2e2e', backgroundColor: "#CCCCCC", borderColor: "#734BF4", borderWidth: "2px" }} type = "text" className = "w-65 rounded-lg p-2"/>
+              <label for="phone" className = "input-label" style = {{fontFamily: 'Inter, sans-serif'}}>Phone Number:</label>
+              <input style={{...columnItemStyle, boxShadow: '0px 1px 3px #2e2e2e', backgroundColor: "#CCCCCC", borderColor: "#734BF4", borderWidth: "2px", fontFamily: 'Inter, sans-serif'}} type = "text" className = "w-65 rounded-lg p-2"/>
 
-              <label for="goalWeight" className = "input-label">Goal Weight:</label>
-              <select style={{...columnItemStyle, boxShadow: '0px 1px 3px #2e2e2e', backgroundColor: "#CCCCCC", borderColor: "#734BF4", borderWidth: "2px" }} type = "text" className = "w-65 rounded-lg p-2">
+              <label for="goalWeight" className = "input-label" style = {{fontFamily: 'Inter, sans-serif'}}>Goal Weight:</label>
+              <select style={{...columnItemStyle, boxShadow: '0px 1px 3px #2e2e2e', backgroundColor: "#CCCCCC", borderColor: "#734BF4", borderWidth: "2px", fontFamily: 'Inter, sans-serif'}} type = "text" className = "w-65 rounded-lg p-2">
                 <option>Select -Goal Weight-</option>
               </select>
 
-              <label for="activity" className = "input-label">Activity Level:</label>
-              <select style={{...columnItemStyle, boxShadow: '0px 1px 3px #2e2e2e', backgroundColor: "#CCCCCC", borderColor: "#734BF4", borderWidth: "2px" }} type = "text" className = "w-65 rounded-lg p-2">
+              <label for="activity" className = "input-label" style = {{fontFamily: 'Inter, sans-serif'}}>Activity Level:</label>
+              <select style={{...columnItemStyle, boxShadow: '0px 1px 3px #2e2e2e', backgroundColor: "#CCCCCC", borderColor: "#734BF4", borderWidth: "2px", fontFamily: 'Inter, sans-serif'}} type = "text" className = "w-65 rounded-lg p-2">
                 <option>Select -Activity Level-</option>
               </select>
 
-              <label for="fitness" className = "input-label">Fitness Experience</label>
-              <select style={{...columnItemStyle, boxShadow: '0px 1px 3px #2e2e2e', backgroundColor: "#CCCCCC", borderColor: "#734BF4", borderWidth: "2px" }} type = "text" className = "w-65 rounded-lg p-2">
+              <label for="fitness" className = "input-label" style = {{fontFamily: 'Inter, sans-serif'}}>Fitness Experience</label>
+              <select style={{...columnItemStyle, boxShadow: '0px 1px 3px #2e2e2e', backgroundColor: "#CCCCCC", borderColor: "#734BF4", borderWidth: "2px", fontFamily: 'Inter, sans-serif'}} type = "text" className = "w-65 rounded-lg p-2">
                 <option>Select -Fitness Experience-</option>
               </select>
 
@@ -127,13 +129,21 @@ const AddClient = () => {
         </div>
       </div>  
       
-      <div style={circleStyle}></div>
-      <div style={rectangleStyle}>
-        <label className = "input-label text-right mr-3 mt-2">Edit Color</label>
+      <div style={circleStyle}>
+        <div style={rectangleStyle}>
+          <img src={pencil_icon}></img>
+          <label className = "input-label text-black text-right mr-3 mt-2" style = {{fontFamily: 'Inter, sans-serif'}}>Edit Color</label>
+        </div>
       </div>
+      
        
     </div>
   )
 }
 
 export default AddClient;
+
+
+/* <div style={rectangleStyle}>
+        <label className = "input-label text-right mr-3 mt-2" style = {{fontFamily: 'Inter, sans-serif'}}>Edit Color</label>
+      </div>*/
